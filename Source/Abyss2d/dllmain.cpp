77e3e0d3b189120414d9c23a98f2137f6f259e67
lekrieg <danielhenriquesilva2010@hotmail.com>
2023-09-14 09:54:10 -0300

    update 3rdParty and some config

--- Source/Abyss2d/dllmain.cpp
+++ Source/Abyss2d/dllmain.cpp
@@ -1,4 +1,4 @@
-#include <windows.h>
+ #include <windows.h>
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
@@ -13,4 +13,3 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 	
     return TRUE;
 }
-
